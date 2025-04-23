9f5ee3007d7b30bb160f98b1f8db5d1a52218aed
naiiairaa <nailakhairaathifah@gmail.com>
2025-04-23 15:02:49 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("naila pake i bukan y")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "nai pinky[docs.streamlit.io](https://docs.streamlit.io/)."
 )
